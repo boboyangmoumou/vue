f27c8ee479c638ef4b2047e7c146eb59a9dbe8d2
杨金海 <18229635207@163.com>
2017-05-12 17:19:53 +0800

    vue components update

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -16,11 +16,24 @@
                         <span class="now">￥{{food.price}}</span>
                         <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                     </div>
+                      <div class="cartcontrol-wrapper">
+                        <cartcontrol :food="food"></cartcontrol>
+                      </div>
+                    <div class="buy" @click="addFirst(food,$event)"
+                    v-show="!food.count || food.count===0">加入购物车
+                    </div>
+                </div>       
+                <split></split>
+                <div class="info" v-show="food.info">
+                    <h1 class="title">商品介绍</h1>
+                    <p class="text">{{food.info}}</p>
                 </div>
-                <div class="cartcontrol-wrapper">
-                    <cartcontrol :food="food"></cartcontrol>
+                <split></split>
+                <div class="rating">
+                    <h1 class="title">商品评价</h1>
+                    <ratingselect :select-type="selectType"  :only-content="onlyContent"
+                    :desc="desc" :ratings="food.ratings"></ratingselect>
                 </div>
-                <div class="buy" @click="addFirst(food,$event)" v-show="!food.count || food.count===0">加入购物车</div>
             </div>
          </div>
     </transition>
@@ -28,6 +41,9 @@
 </template>
 <script>
     import BScroll from 'better-scroll';
+    import Vue from 'vue';
+    import split from 'components/split/split';
+    import ratingselect from 'components/ratingselect/ratingselect';
     import cartcontrol from 'components/cartcontrol/cartcontrol';
     export default {
         props: {
@@ -37,12 +53,21 @@
         },
         data() {
             return {
-                showFlag: false
+                showFlag: false,
+                // selectType:ALL,
+                onlyContent:true,
+                desc:{
+                    all:'全部',
+                    positive:'推荐',
+                    negative:'吐槽'
+                }
             };
         },
         methods: {
             show() {
                 this.showFlag = true;
+                this.selectType = 1;
+                this.onlyContent = 2;
                 this.$nextTick(() => {
                     if (!this.scroll) {
                         this.scroll = new BScroll(this.$refs.food, {
@@ -55,10 +80,22 @@
             },
             hide() {
                 this.showFlag = false;
+            },
+            addFirst(){
+                if (!event._constructed) {
+                    return;
+                }
+                 if (!this.food.count) {
+                    Vue.set(this.food, 'count', 1);
+                } else {
+                    this.food.count++;
+                }
             }
         },
         components: {
-            cartcontrol
+            cartcontrol,
+            split,
+            ratingselect
         }
     };
 </script>
@@ -113,6 +150,7 @@
     
     .food-content .content {
         padding: 18px;
+        position:relative;
     }
     
     .food-content .content .title {
@@ -161,6 +199,7 @@
         position: absolute;
         right: 12px;
         bottom: 12px;
+        z-index:3;
     }
     
     .food-content .buy {
@@ -176,5 +215,20 @@
         border-radius: 10px;
         color: #fff;
         background: rgb(0, 160, 220);
+    }
+    .food-content .info{
+        padding: 18px;
+    }
+    .food-content .info .title{
+        line-height: 14px;
+        margin-bottom: 6px;
+        font-size: 14px;
+        color: rgb(7,17,27);
+    }
+    .food-content .info .text{
+        line-height: 24px;
+        padding: 0 8px;
+        font-size: 12px;
+        color: rgb(77,85,93);
     }
 </style>
